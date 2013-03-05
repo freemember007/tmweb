5ff3f6ab2755b5a0ccb1b5845a2a43857d395438
freemem <freemember007@gmail.com>
2013-03-05 16:42:13 +0800

    alt login_api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,7 +8,26 @@ class ApiController < ApplicationController
       render :json => {:type => :fail}
       return
     end
-    
+    if u.valid_password?(params[:password])
+      data = []
+      lastday = u.items.first.created_at
+      Rails.logger.info u.items.first
+      Rails.logger.info lastday
+      @items = u.items.from_month(lastday.year, lastday.month).group_by{|item| item.created_at.to_date }
+      render :json => {:type => :success, :id => u.id, :email => params[:email], :password => params[:password], :items => @items}
+      return
+    else
+      render :json => {:type => :fail}
+      return
+    end
+  end
+  
+  def login_bak
+    u = User.find_for_database_authentication(:email=>params[:email])
+    if u.nil?
+      render :json => {:type => :fail}
+      return
+    end
     if u.valid_password?(params[:password])
       data = []
       lastday = u.items.first.created_at
@@ -23,33 +42,6 @@ class ApiController < ApplicationController
           content = item.content if !item.content.nil?
           data << {:content => content, :image => url, :date => item.created_at.strftime("%Y/%m/%d") , :index => index}
         end
-      end
-      render :json => {:type => :success, :id => u.id, :email => params[:email], :password => params[:password], :items => data}
-      return
-    else
-      render :json => {:type => :fail}
-      return
-    end
-  end
-  
-  def login_bak
-    u = User.find_for_database_authentication(:email=>params[:email])
-    if u.nil?
-      render :json => {:type => :fail}
-      return
-    end
-    
-    if u.valid_password?(params[:password])
-      data = []
-      lastday = u.items.first.created_at
-      Rails.logger.info u.items.first
-      Rails.logger.info lastday
-      u.items.from_day(lastday.year, lastday.month, lastday.day).each do |item|
-        url = "null"
-        url = item.images.first.url if item.images.any?
-        content = "null"
-        content = item.content if !item.content.nil?
-        data << {:content => content, :image => url, :date => item.created_at.strftime("%Y/%m/%d")}
       end
       render :json => {:type => :success, :id => u.id, :email => params[:email], :password => params[:password], :items => data}
       return
